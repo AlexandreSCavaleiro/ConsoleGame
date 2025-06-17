7ae2483d7cd0bc522329b108e570876ea21898f1
AlexandreSCavaleiro <alexandre_cavaleiro@outlook.com>
2025-06-17 12:11:18 -0300

    Feat: Classe Drop (old)

--- ProjetoUC/Drop.cs
+++ ProjetoUC/Drop.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace ProjetoUC
 {
-    internal class Drop
+    public class Drop
     {
+        public String nome { get; set; }
+        public int id { get; set; }
+        public int valor { get; set; }
+
+        public Drop(string nome, int id, int valor)
+        {
+            this.nome = nome;
+            this.id = id;
+            this.valor = valor;
+        }
+
+        public void showAll()
+        {
+            Console.WriteLine($"""
+                    [{nome} | {id} | {valor}]
+                """);
+        }
+
+        public void show()
+        {
+            Console.WriteLine($"""
+                {nome} | +{valor}
+                """);
+        }
+
+        public String toString()
+        {
+            string retorno = $"{nome} | {id} | {valor}";
+
+            return retorno;
+        }
     }
 }
