df82fd95d0fce2041fa19c289748ac97ad4e0770
AlexandreSCavaleiro <alexandre_cavaleiro@outlook.com>
2025-06-17 12:09:09 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoUC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoUC
+{
+    class Program
+    {
+
+
+        public static void Main()
+        {
+            
+        }
+    }
+}
