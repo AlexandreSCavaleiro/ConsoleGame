b99470fdd82290a878bc03509776cd1d5c9fabd1
alexandreCavaleiro <alexandre_cavaleiro@outlook.com>
2025-07-01 14:12:40 -0300

    Feat: Criação da classe pixel

---
+++ ProjetoUC/Pixel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoUC
+{
+    class Pixel
+    {
+        private ConsoleColor cor;
+        char icone;
+
+        public Pixel(ConsoleColor cor, char icone)
+        {
+
+            this.cor = cor;
+            this.icone = icone;
+        }
+
+        public void show()
+        {
+            Console.ForegroundColor = this.cor; 
+            Console.Write(this.icone);
+        }
+    }
+}
