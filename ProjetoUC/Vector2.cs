6bae5f88f2b2c58c0a45ec84c30b2a024ee10735
AlexandreCavaleiro <alexandre_cavaleiro@outlook.com>
2025-07-21 16:40:38 -0300

    Feat: funcao distancia 2 pontos

--- ProjetoUC/Vector2.cs
+++ ProjetoUC/Vector2.cs
@@ -24,5 +24,20 @@ namespace ProjetoUC
         public int Left => this.x -= 1;
         public int Rigth => this.x += 1;
 
+        //funcao que retorna a distancia entre dois
+        public static int Dinstance(Vector2 a, Vector2 b)
+        {
+            if (a == null) return -1; 
+            if (b == null) return -1; 
+
+            return (
+                (int)Math.Sqrt(
+                    (a.x - b.x)*(a.x - b.x) + 
+                    (a.y - b.y)*(a.y - b.y)
+                )
+            );
+        }
+
+
     }
 }
