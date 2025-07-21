a98b9b4ef012271f1c22bf0c9a272fd22e4fafe2
AlexandreCavaleiro <alexandre_cavaleiro@outlook.com>
2025-07-21 16:27:06 -0300

    Feat: construindo Vector2(classe na unity)

---
+++ ProjetoUC/Vector2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoUC
+{
+    public class Vector2
+    {
+        //attr
+        public int x, y;
+        
+        //constructor
+        public Vector2(int x, int y)
+        {
+            this.x = x; 
+            this.y = y;
+        }
+
+        //funcoes de movimento
+        public int Up => this.y -= 1;
+        public int Down => this.y += 1;
+        public int Left => this.x -= 1;
+        public int Rigth => this.x += 1;
+
+    }
+}
